a0a091c2b9ba9d2aa38607e21ce2b5045f302e0a
Lindsey Stephens <lindseystep@gmail.com>
2018-09-13 03:28:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ const speak = () => {
   }
   if (textInput.value !== '') {
     // Add background animation
-    body.style.background = '#141414 url(Assets/wave.gif)';
+    body.style.background = '#141414 url(/wave.gif)';
     body.style.backgroundRepeat = 'repeat-x';
     body.style.backgroundSize = '100% 100%';
 
